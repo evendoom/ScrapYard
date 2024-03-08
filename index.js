ca45eddf70a18eecec2c3fc64d4c6ec3b1339824
evendoom <davidej.p.correia@gmail.com>
2024-03-08 15:05:05 +0000

    feat: Set up basic route and ejs templates

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const ejs = require('ejs')
+const ejsMate = require('ejs-mate')
+
+// Configure app to use EJS as view engine
+app.set('view engine', 'ejs')
+
+// Configure app to use 'views' folder as default for templates
+app.set('views', path.join(__dirname, 'views'))
+
+// Configure app to use EJS boilerplates
+app.engine('ejs', ejsMate)
+
+// Configure app to use public directory (css, ejs...)
+app.use(express.static('public'))
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+// App listening
+app.listen(3000, () => {
+    console.log('Listening on port 3000...')
+})
